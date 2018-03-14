fd66525453f4f589212a02c200fcdb7d32c642d2
Chelsea G <chelseagieck@Chelseas-MacBook-Pro.local>
2018-03-14 19:17:36 -0400

    class push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,27 +28,33 @@ var guessReset = function() {
   var updateUserGuesses = function() {
     document.getElementById('userLetterGuess').innerHTML = userGuessList;
   };
+  /*
   var wordPush = function(){
       if ( 
           userKey === "a" || userKey === "b" || userKey === "c" || userKey === "d" || userKey === "e" || userKey === "f" || userKey === "g" || userKey === "h" || userKey === "i" || userKey === "j" || userKey === "k" || userKey === "l" || userKey === "m" || userKey === "n" || userKey === "o" || userKey === "p" || userKey === "q" || userKey === "r" || userKey === "s" || userKey === "t" || userKey === "u" || userKey === "v" || userKey === "w" || userKey === "x" || userKey === "y" || userKey === "z"
       ){
-          for (i = 0; i < arrNewChoice.length; i++){
-              if (keyPress === blankHang[i]) {
-                  blankHang[i] = keyPress;
-              }
-            }
-        }
+          for (i = 0; i < wordChoice.length; i++){
+              if (userKey === wordChoice[i]) {
+                  blankHang[i] = userKey;
+              } else{
+                  console.log('no changes');
+              };
+            };
+        };
     };
+    */
 
 
   document.getElementById('start').addEventListener("click", function(){
       random = Math.floor(Math.random() * wordBank.length);
       var newChoice = wordBank[random];
       console.log(newChoice); 
+      var blankHang = ''.padStart('_', newChoice.length);
 
       guessReset();
       document.getElementById('userWins').innerHTML = wins;
       document.getElementById('userLosses').innerHTML = losses;
+    
 
       var arrNewChoice = newChoice.split("");
       console.log(arrNewChoice);
@@ -58,13 +64,12 @@ var guessReset = function() {
 
       document.getElementById("word").innerHTML = blankHang;
 
-/*      for (i = 0; i< newChoice.length ; i++) {
+      for (i = 0; i< newChoice.length ; i++) {
             pushChar = newChoice[i];
             console.log (pushChar);
             blankHang.push(pushChar);
         };
-        
-    */
+
 
 
         document.onkeydown = function(event){
